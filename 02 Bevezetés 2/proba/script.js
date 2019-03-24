5d9a69178e727e468b2e4e57674a85e3d4a60759
kiss j gabi <b6@sryer.com>
2019-03-24 13:23:35 +0100

    add 02 Bevezetés 2 - innerHTML

---
+++ 02 Bevezetés 2/proba/script.js
@@ -0,0 +1,8 @@
+// Külső JavaScript fájl
+
+document.getElementById("gomb").onclick = function () {
+    document.getElementById("szoveg").innerHTML = "Valami más.";
+}
+document.getElementById("gomb2").onclick = function () {
+    document.getElementById("szoveg").innerHTML = "&nbsp;";
+}
